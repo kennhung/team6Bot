c16c02a19711a59c75ac221d780c34c703d38512
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-08-09 13:15:20 +0800

    [add] finish jszhuyin api server

---
+++ index.js
@@ -0,0 +1,33 @@
+const { JSZhuyin } = require('jszhuyin');
+const http = require('http');
+
+var jszhuyin = new JSZhuyin();
+jszhuyin.load();
+
+const server = http.createServer((request, response) => {
+    if (request.method != 'POST') {
+        response.writeHead(405);
+        response.end('ERROR 405');
+        return
+    }
+
+    let body = '';
+
+    request.on('data', function (data) {
+        body += data;
+    });
+
+    request.on('end', function () {
+        console.log('Body: ' + body);
+
+        jszhuyin.oncandidateschange = function (c) {
+            response.writeHead(200, { 'Content-Type': 'text/plain' });
+            response.end(c[0][0]);
+        };
+
+        jszhuyin.handleKey(body);
+    });
+});
+
+server.listen(8666);
+console.log('server started at port 8666')
