5a7677a816b29e8a15060157976801e6a2dafd00
KennHuang <j20281@my.cmsh.cyc.edu.tw>
2019-08-10 19:33:30 +0800

    [add] add zhuyin module

---
+++ app/modules/zhuyin.py
@@ -0,0 +1,49 @@
+import requests
+from zhuyin_engkey_dict import eng_zhuyin_dict
+import random
+
+reply_sentences = [
+    "你是想說 **{}** 嗎?",
+    "你又忘記切輸入法了啦,幫你翻譯\n**{}**",
+    ""
+]
+
+wrong_count = {}
+
+def zhuyin_to_words(zhuyin):
+    r = requests.post('http://zhuyin.yubin.tw', json={'text': zhuyin})
+    words = r.json()['text']
+    return words
+
+
+def engkey_to_zhuyin(eng_string):
+    zhuyin_string = ''
+    for c in eng_string:
+        # print('{}: {}'.format(c, eng_zhuyin_dict[c]));
+        zhuyin_string += eng_zhuyin_dict[c]
+    return zhuyin_string
+
+
+def engkey_to_words(engkey):
+    return zhuyin_to_words(engkey_to_zhuyin(engkey))
+
+
+def handle_tg_message(message, update):
+    # TODO: add vaild check
+    if True:
+        correct_sentence = engkey_to_words(message)
+        if len(correct_sentence) != 0:
+            
+            if update.message.from_user.id not in wrong_count:
+                wrong_count[update.message.from_user.id] = 0
+            wrong_count[update.message.from_user.id] += 1
+            print('{}: {}'.format(update.message.from_user.id, wrong_count[update.message.from_user.id]))
+
+            reply = random.choice(reply_sentences).format(correct_sentence)
+            update.message.reply_text(reply)
+
+
+if __name__ == '__main__':
+    # run tests
+    print(zhuyin_to_words('ㄨㄛˇㄏㄠˇㄜˋ'))
+    print(engkey_to_words('su3cl3'))
